9dc20984cc9fdc05d6f91ea6f152cb2df20355fa
FabianEP <fabian.pellegrino@gmail.com>
2018-06-21 23:40:05 -0300

    Mejora de interfaz de usuario en modulo de seguridad registrase, login, reset password, base page, menu page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.baseapp')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+@section('body_class', 'gray-bg')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+@section('maincontent')
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+                <h1 class="logo-name">IN+</h1>
             </div>
+            <h3>Welcome to IN+</h3>
+            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.</p>
+            <p>Login in. To see it in action.</p>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div class="form-group row">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="{{ __('E-Mail Address') }}" required autofocus>
+
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group row">
+                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" required>
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group row">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                        <button type="submit" class="btn btn-primary block full-width m-b">
+                            {{ __('Login') }}
+                        </button>
+
+                        <a class="" href="{{ route('password.request') }}">
+                            <small>{{ __('Forgot Your Password?') }}</small>
+                        </a>
+                        <p class="text-muted text-center"><small>Do not have an account?</small></p>
+                        <a class="btn btn-sm btn-white btn-block" href="{{ route('register') }}">
+                            <!--{{ __('register') }}-->
+                            Create an account
+                        </a>
+                </div>
+            </form>
+            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/appdiscv.blade.php
+++ resources/views/layouts/appdiscv.blade.php
@@ -26,8 +26,7 @@
     
         <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
 
-        <!-- Toastr style -->
-        <link href="{{ asset('css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
+        @yield('styles')
 
         <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
         <link href="{{ asset('css/template-style.css') }}" rel="stylesheet">
@@ -55,7 +54,13 @@
                                     <li><a href="contacts.html">Contacts</a></li>
                                     <li><a href="mailbox.html">Mailbox</a></li>
                                     <li class="divider"></li>
-                                    <li><a href="login.html">Logout</a></li>
+                                    <li>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+                                    </li>
                                 </ul>
                             </div>
                             <div class="logo-element">
@@ -268,7 +273,6 @@
         
                 </div>
             </nav>
-        
             <div id="page-wrapper" class="gray-bg">
                 <div class="row border-bottom">
                     <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
@@ -382,9 +386,14 @@
         
         
                             <li>
-                                <a href="login.html">
-                                    <i class="fa fa-sign-out"></i> Log out
-                                </a>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                  document.getElementById('logout-form').submit();">
+                                    <i class="fa fa-sign-out"></i> {{ __('Logout') }}
+                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                </form>
                             </li>
                         </ul>
         
@@ -780,16 +789,11 @@
         
             </div>
         </div>
-
+        
         <!-- Mainly scripts -->
         <script src="{{ asset('js/jquery-2.1.1.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
-        <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
-        <!-- Custom and plugin javascript -->
-        <script src="{{ asset('js/inspinia.js') }}"></script>
-        <script src="{{ asset('js/plugins/pace/pace.min.js')}} "></script>
-
+        @yield('jsscript')
     </body>
 </html>
