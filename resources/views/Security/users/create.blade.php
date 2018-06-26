38aa88e50b9570e4cb9365b7a0f6f4b4721519c5
FabianEP <fabian.pellegrino@gmail.com>
2018-06-25 22:41:33 -0300

    Mejorando interfaz de modulo de gestion de Usuario

--- resources/views/Security/users/create.blade.php
+++ resources/views/Security/users/create.blade.php
@@ -1,50 +1,118 @@
-@extends('layouts.app')
+@extends('layouts.menuapp')
 
 @section('title', '| Add User')
 
+@section('headercontent')
+    <h2><i class='fa fa-user-plus'></i> Add User</h2>
+    <ol class="breadcrumb">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a>Security</a>
+        </li>
+        <li class="active">
+            <strong>Add User</strong>
+        </li>
+    </ol>
+@endsection
+
 @section('content')
 
-<div class='col-lg-4 col-lg-offset-4'>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h5><i class='fa fa-user-plus'></i> Add User <small></small></h5>
+                </div>
+                <div class="ibox-content">
+                    {{ Form::open(array('url' => 'security/users', 'class' => 'form-horizontal')) }}
 
-    <h1><i class='fa fa-user-plus'></i> Add User</h1>
-    <hr>
+                    <div class="form-group">
+                        {{ Form::label('name', 'Name', array('class' => 'col-sm-2 control-label')) }}
+                        <div class="col-sm-10">
+                            {{ Form::text('name', '', array('class' => 'form-control')) }}
+                        </div>
+                    </div>
+                    
+                    <div class="hr-line-dashed"></div>
 
-    {{ Form::open(array('url' => 'security/users')) }}
+                    <div class="form-group">
+                        {{ Form::label('email', 'Email', array('class' => 'col-sm-2 control-label')) }}
+                        <div class="col-sm-10">
+                            {{ Form::email('email', '', array('class' => 'form-control')) }}
+                        </div>
+                    </div>
+                
+                    <div class="hr-line-dashed"></div>
+                    
+                    <div class='form-group'>
+                        <label class="col-sm-2 control-label">Roles<br/>
+                        <small class="text-navy">Custom elements</small></label>
+                        
+                        <div class="col-sm-10">
+                            @foreach ($roles as $role)
+                                <div class="i-checks">
+                                    <label> 
+                                        {{ Form::checkbox('roles[]',  $role->id ) }}
+                                        <i></i> {{ Form::label($role->name, ucfirst($role->name)) }}
+                                    </label>
+                                </div>                    
+                            @endforeach
+                        </div>
+                    </div>
+                
+                    <div class="hr-line-dashed"></div>
 
-    <div class="form-group">
-        {{ Form::label('name', 'Name') }}
-        {{ Form::text('name', '', array('class' => 'form-control')) }}
+                    <div class="form-group">
+                        {{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }}<br>
+                        <div class="col-sm-10">
+                            {{ Form::password('password', array('class' => 'form-control')) }}
+                        </div>
+                    </div>
+                
+                    <div class="hr-line-dashed"></div>
+
+                    <div class="form-group">
+                        {{ Form::label('password', 'Confirm Password', array('class' => 'col-sm-2 control-label')) }}<br>
+                        <div class="col-sm-10">
+                            {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
+                        </div>
+                    </div>
+                
+                    <div class="form-group">
+                        <div class="col-sm-4 col-sm-offset-2">
+                            <!-- <button class="btn btn-white" type="submit">Cancel</button> -->
+                            {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
+                        </div>
+                    </div>
+
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="form-group">
-        {{ Form::label('email', 'Email') }}
-        {{ Form::email('email', '', array('class' => 'form-control')) }}
-    </div>
-
-    <div class='form-group'>
-        @foreach ($roles as $role)
-            {{ Form::checkbox('roles[]',  $role->id ) }}
-            {{ Form::label($role->name, ucfirst($role->name)) }}<br>
-
-        @endforeach
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('password', 'Password') }}<br>
-        {{ Form::password('password', array('class' => 'form-control')) }}
-
-    </div>
-
-    <div class="form-group">
-        {{ Form::label('password', 'Confirm Password') }}<br>
-        {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
-
-    </div>
-
-    {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
-
-    {{ Form::close() }}
-
 </div>
 
+@endsection
+
+@section('jsscript')
+    <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+    <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{ asset('js/inspinia.js') }}"></script>
+    <script src="{{ asset('js/plugins/pace/pace.min.js')}} "></script>
+
+    <!-- iCheck -->
+    <script src="js/plugins/iCheck/icheck.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.i-checks').iCheck({
+                checkboxClass: 'icheckbox_square-green',
+                radioClass: 'iradio_square-green',
+            });
+        });
+    </script>
 @endsection
--- resources/views/Security/users/index.blade.php
+++ resources/views/Security/users/index.blade.php
@@ -1,51 +1,106 @@
-@extends('layouts.app')
+@extends('layouts.menuapp')
 
 @section('title', '| Users')
 
+@section('headercontent')
+    <h2><i class="fa fa-users"></i> User Administration</h2>
+    <ol class="breadcrumb">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a>Security</a>
+        </li>
+        <li class="active">
+            <strong>User Administration</strong>
+        </li>
+    </ol>
+@endsection
+
 @section('content')
 
-<div class="col-lg-10 col-lg-offset-1">
-    <h1><i class="fa fa-users"></i> User Administration <a href="{{ route('roles.index') }}" class="btn btn-default pull-right">Roles</a>
-    <a href="{{ route('permissions.index') }}" class="btn btn-default pull-right">Permissions</a></h1>
-    <hr>
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped">
-
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Date/Time Added</th>
-                    <th>User Roles</th>
-                    <th>Operations</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($users as $user)
-                <tr>
-
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->created_at->format('F d, Y h:ia') }}</td>
-                    <td>{{  $user->roles()->pluck('name')->implode(' ') }}</td>{{-- Retrieve array of roles associated to a user and convert to string --}}
-                    <td>
-                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-info pull-left" style="margin-right: 3px;">Edit</a>
-
-                    {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id] ]) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
-
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-
-        </table>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5><i class="fa fa-users"></i> User Administration</h5>
+            </div>
+            <div class="ibox-content">
+                <div class="row">
+                    <div class="col-md-2">
+                            <a href="{{ route('users.create') }}" class="btn btn-success">Add User</a>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control input-sm m-b-xs" id="filter" placeholder="Search in table">
+                    </div>
+                    <div class="col-md-4">
+                        <a href="{{ route('roles.index') }}" class="btn btn-default pull-right">Roles</a>
+                        <a href="{{ route('permissions.index') }}" class="btn btn-default pull-right">Permissions</a>
+                    </div>
+                </div>
+                <table class="footable table table-stripped" data-page-size="8" data-filter=#filter>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Date/Time Added</th>
+                            <th>User Roles</th>
+                            <th>Operations</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                        <tr>
+        
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->created_at->format('F d, Y h:ia') }}</td>
+                            <td>{{  $user->roles()->pluck('name')->implode(' ') }}</td>{{-- Retrieve array of roles associated to a user and convert to string --}}
+                            <td>
+                            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-info pull-left" style="margin-right: 3px;">Edit</a>
+        
+                            {!! Form::open(['method' => 'DELETE', 'route' => ['users.destroy', $user->id] ]) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                            {!! Form::close() !!}
+        
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="5">
+                                <ul class="pagination pull-right"></ul>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>    
+                
+            </div>
+        </div>
     </div>
-
-    <a href="{{ route('users.create') }}" class="btn btn-success">Add User</a>
-
 </div>
+@endsection
 
+@section('jsscript')
+    <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+    <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+
+    <!-- FooTable -->
+    <script src="{{ asset('js/plugins/footable/footable.all.min.js') }}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{ asset('js/inspinia.js') }}"></script>
+    <script src="{{ asset('js/plugins/pace/pace.min.js')}} "></script>
+
+    <!-- Page-Level Scripts -->
+    <script>
+        $(document).ready(function() {
+
+            $('.footable').footable();
+            $('.footable2').footable();
+
+        });
+
+    </script>
 @endsection
